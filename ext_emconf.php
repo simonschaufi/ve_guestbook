6ca8c2ad9e6e3a13791b3166bce61928d86173ff
Simon Schaufelberger <simonschaufi@googlemail.com>
2017-03-15 01:39:13 +0100

    first commit

---
+++ ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "ve_guestbook".
+ *
+ * Auto generated 04-09-2013 13:29
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Modern Guestbook / Commenting system',
+	'description' => 'Many features. Easy installation. Mature extension - actually no known bug - see bugs.typo3.org.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '2.12.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => 'uploads/tx_veguestbook/rte/',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Udo von Eynern',
+	'author_email' => 'udo@voneynern.de',
+	'author_company' => 'Freelancer Udo von Eynern',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+		),
+		'conflicts' => '',
+		'suggests' => 
+		array (
+		),
+	),
+);
+
+?>
